2607c294f095854581f4b0b2e08387506b400475
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-05-21 16:38:12 -0300

    Listando

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function fetchApiData(){
- fetch('https://jsonplaceholder.typicode.com/todos/').then((response)=>{
-   response.json();
+ fetch('https://jsonplaceholder.typicode.com/todos/')
+ .then(response => response.json())
+ .then(data =>{
+ 
+   const list = document.querySelector('#fill_list')
+   data.map((item) => {
 
-   
+      const li = document.createElement('li');
+      li.setAttribute('id',item.id)
+      li.innerHTML = item.title;
+      list.appendChild(li);
+   })
 
 
 
-
- })
-
-
-
-}
+ });
+};
